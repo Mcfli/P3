db357c1a5d03eab8488d36a0c4dcccaafee8581e
Nathan Irwin <nki818@mac.com>
2015-04-20 15:22:25 -0700

    added bidirectional A*

--- p3_pathfinder.py
+++ p3_pathfinder.py
@@ -3,22 +3,29 @@ from math import sqrt
 
 def find_path(source_point, destination_point, mesh):
 
-    # Need to implement bidirectional, A* seems to be working
-
     def AStar(src, dst, graph):
-        dist = {}
-        prev = {}
+        forward_dist = {}
+        backward_dist = {}
+        forward_prev = {}
+        backward_prev = {}
         detail_points = {}
         queue = []
         detail_points[src] = source_point
         detail_points[dst] = destination_point
-        dist[src] = 0
-        prev[src] = None
-        heappush(queue, (dist[src], src))
+        forward_dist[src] = 0
+        backward_dist[dst] = 0
+        forward_prev[src] = None
+        backward_prev[dst] = None
+        tentative = 0
+        nextDist = 0
+        heappush(queue, (forward_dist[src], src, 'destination'))
+        heappush(queue, (backward_dist[dst], dst, 'source'))
 
         while queue:
-            _, node = heappop(queue)
-            if node == dst:
+            _, node, curr_goal = heappop(queue)
+            if curr_goal == 'destination' and node in backward_prev:
+                break
+            if curr_goal == 'source' and node in forward_prev:
                 break
 
             neighbors = graph['adj'].get(node, []) # method from piazza
@@ -29,22 +36,39 @@ def find_path(source_point, destination_point, mesh):
                 # next box formula from piazza
                 nextPos = (min(nextX2-1,max(nextX1,nodeX)), min(nextY2-1,max(nextY1,nodeY)))
                 detail_points[next_node] = nextPos
-                tentative = dist[node] + eucDist(detail_points[node], detail_points[next_node])
-                if next_node not in dist or tentative < dist[next_node]:
-                    visited_nodes.append(next_node)
-                    dist[next_node] = tentative
-                    prev[next_node] = node
-                    heappush(queue, (tentative + eucDist(detail_points[next_node], destination_point), next_node))
+                if curr_goal == 'destination':
+                    tentative = forward_dist[node] + eucDist(detail_points[node], detail_points[next_node])
+                else:
+                    tentative = backward_dist[node] + eucDist(detail_points[node], detail_points[next_node])
+                if curr_goal == 'destination':
+                    if next_node not in forward_dist or tentative < forward_dist[next_node]:
+                        visited_nodes.append(next_node)
+                        forward_dist[next_node] = tentative
+                        forward_prev[next_node] = node
+                        heappush(queue, (tentative + eucDist(detail_points[next_node], destination_point), next_node, 'destination'))
+                else:
+                    if next_node not in backward_dist or tentative < backward_dist[next_node]:
+                        visited_nodes.append(next_node)
+                        backward_dist[next_node] = tentative
+                        backward_prev[next_node] = node
+                        heappush(queue, (tentative + eucDist(detail_points[next_node], source_point), next_node, 'source'))
 
-        if node == dst:
+        if node in backward_prev and node in forward_prev:
             path = []
             detail_points[src] = source_point
             detail_points[dst] = destination_point
+            
+            nodeCopy = node
             while node:
-                prevNode = prev[node]
+                prevNode = forward_prev[node]
                 if prevNode is not None:
                     path.append((detail_points[node], detail_points[prevNode]))
                 node = prevNode
+            while nodeCopy:
+                prevNode = backward_prev[nodeCopy]
+                if prevNode is not None:
+                    path.append((detail_points[nodeCopy], detail_points[prevNode]))
+                nodeCopy = prevNode
             return path
         else:
             return []
